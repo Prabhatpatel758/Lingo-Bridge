3c5416251c3f4270305610d63a211bf55c1608f9
Prabhatpatel758 <prabhatpatel758@gmail.com>
2021-05-02 20:52:24 +0530

    plans and pricing update
    plans and pricing update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,35 @@
+import { AppRoutingModule } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ChartsModule } from 'ng2-charts';
+
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
-
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
-
 import { IconModule, IconSetModule, IconSetService } from '@coreui/icons-angular';
 
-const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
-  suppressScrollX: true
-};
-
 import { AppComponent } from './app.component';
-
-// Import containers
+import { HomeComponent } from './landingcomponents/home/home.component';
+import { LandingComponent } from './landingcomponents/landing/landing.component';
+import { LandingfooterComponent } from './landingcomponents/landingfooter/landingfooter.component';
+import { PlansandpricingComponent } from './landingcomponents/plansandpricing/plansandpricing.component';
+import { TeachwithusComponent } from './landingcomponents/teachwithus/teachwithus.component';
+import { NavbarmainComponent } from './landingcomponents/navbarmain/navbarmain.component';
 import { DefaultLayoutComponent } from './containers';
 
 import { P404Component } from './views/error/404.component';
 import { P500Component } from './views/error/500.component';
 import { LoginComponent } from './landingcomponents/login/login.component';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 const APP_CONTAINERS = [
   DefaultLayoutComponent
@@ -35,20 +42,6 @@ import {
   AppFooterModule,
   AppSidebarModule,
 } from '@coreui/angular';
-
-// Import routing module
-import { AppRoutingModule } from './app.routing';
-
-// Import 3rd party components
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { ChartsModule } from 'ng2-charts';
-import { HomeComponent } from './landingcomponents/home/home.component';
-import { LandingComponent } from './landingcomponents/landing/landing.component';
-import { LandingfooterComponent } from './landingcomponents/landingfooter/landingfooter.component';
-import { PlansandpricingComponent } from './landingcomponents/plansandpricing/plansandpricing.component';
-import { TeachwithusComponent } from './landingcomponents/teachwithus/teachwithus.component';
-import { NavbarmainComponent } from './landingcomponents/navbarmain/navbarmain.component';
 
 @NgModule({
   imports: [
