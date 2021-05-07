125a8218ee9304c9e03244a77cce41087e70f994
Prabhatpatel758 <prabhatpatel758@gmail.com>
2021-05-08 00:43:10 +0530

    change password uia
    change password uia

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
+  user = { fullName: 'Prabhat Patel', emilId: 'abc@gmail.com', qualification: 'btech', profession: 'product developer' };
+  cred = { oldPassword: '', newPassword: '', confirmPassword: '' };
+  isDisabled: boolean = true;
+  isChangePassword: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  enableForm = () => {
+    this.isDisabled = false;
+  }
+  changePassword = () => {
+    console.log('hi');
+    this.isChangePassword = !this.isChangePassword;
+  }
 }
